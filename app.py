d30e9c5d5f9192a2aab74cbd0e3b7916980dcfb9
Moisés Tedeschi <moisestedeschi@gmail.com>
2021-03-01 20:38:14 -0300

    Listando postagens e mostrando por ID.

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import sqlite3
-from sqlite3.dbapi2 import connect
+
 from flask import Flask, render_template
+from werkzeug.exceptions import abort
 
 
 def get_db_connection():
@@ -8,6 +9,18 @@ def get_db_connection():
     conn.row_factory = sqlite3.Row
 
     return conn
+
+
+def get_post(post_id):
+    conn = get_db_connection()
+    post = conn.execute('SELECT * FROM posts WHERE id = ?',
+                        (post_id,)).fetchone()
+
+    conn.close()
+
+    if post is None:
+        abort(404)
+    return post
 
 
 app = Flask(__name__)
@@ -20,6 +33,13 @@ def index():
     conn.close()
 
     return render_template('index.html', posts=posts)
+
+
+@app.route('/<int:post_id>')
+def post(post_id):
+    post = get_post(post_id)
+
+    return render_template('post.html', post=post)
 
 
 if __name__ == "__main__":
